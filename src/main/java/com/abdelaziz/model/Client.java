b5457632be6c2bc15a8645b6f0e838170ea3cf7c
BENJMAA Abdelaziz <abenjmaa@softeam.com>
2018-10-16 17:32:30 +0200

    add hashcode & equal

--- src/main/java/com/abdelaziz/model/Client.java
+++ src/main/java/com/abdelaziz/model/Client.java
@@ -3,6 +3,7 @@ package com.abdelaziz.model;
 import java.util.List;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -16,13 +17,68 @@ public class Client {
 
 	 @Id
 	 @GeneratedValue
-	 @Column(name="id_clt")
+	 @Column(name="id_clt")  
 	 private Long id ;
 	 
 	 @Column(name="clt_name")
 	 private String name;
 	 
-	 @Column(name="clt_login")
+	 @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((listLocation == null) ? 0 : listLocation.hashCode());
+		result = prime * result + ((login == null) ? 0 : login.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (listLocation == null) {
+			if (other.listLocation != null)
+				return false;
+		} else if (!listLocation.equals(other.listLocation))
+			return false;
+		if (login == null) {
+			if (other.login != null)
+				return false;
+		} else if (!login.equals(other.login))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+
+	@Column(name="clt_login")
 	 private String login;
 	 
 	 @Column(name="clt_psw")
--- src/main/java/com/abdelaziz/model/Location.java
+++ src/main/java/com/abdelaziz/model/Location.java
@@ -53,6 +53,48 @@ public class Location {
 		this.prix = prix;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dateLoc == null) ? 0 : dateLoc.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((label == null) ? 0 : label.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(prix);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Location other = (Location) obj;
+		if (dateLoc == null) {
+			if (other.dateLoc != null)
+				return false;
+		} else if (!dateLoc.equals(other.dateLoc))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (label == null) {
+			if (other.label != null)
+				return false;
+		} else if (!label.equals(other.label))
+			return false;
+		if (Double.doubleToLongBits(prix) != Double.doubleToLongBits(other.prix))
+			return false;
+		return true;
+	}
+
 	public Calendar getDateLoc() {
 		return dateLoc;
 	}
