c992e380d7d2004ffb82eea501c863c90d77183f
BENJMAA Abdelaziz <abenjmaa@softeam.com>
2018-10-17 10:23:08 +0200

    Location DAO

--- src/main/java/com/abdelaziz/dao/ClientDAO.java
+++ src/main/java/com/abdelaziz/dao/ClientDAO.java
@@ -1,9 +1,10 @@
 package com.abdelaziz.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import com.abdelaziz.model.Client;
-
+@Repository
 public interface ClientDAO extends PagingAndSortingRepository<Client, Long> {
 
 }
--- src/main/java/com/abdelaziz/dao/LocationDAO.java
+++ src/main/java/com/abdelaziz/dao/LocationDAO.java
@@ -1,9 +1,10 @@
 package com.abdelaziz.dao;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import com.abdelaziz.model.Location;
-
+@Repository
 public interface LocationDAO extends PagingAndSortingRepository<Location, Long> {
 
 }
