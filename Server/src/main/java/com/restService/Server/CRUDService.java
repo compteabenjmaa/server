f4bedb46cea66f4d7a2ee622cbfb8e98e6549ace
abenjmaa <benjmaa.abdelaziz@gmail.com>
2020-04-04 12:29:44 +0100

    simple rest web service

---
+++ Server/src/main/java/com/restService/Server/CRUDService.java
@@ -0,0 +1,105 @@
+package com.restService.Server;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CRUDService {
+
+	private List<Person> listPerson = new ArrayList<Person>();
+	private final AtomicInteger counter = new AtomicInteger();
+
+	@GetMapping("/listPerson")
+	public ResponseEntity<Message> getListPerson() throws JSONException {
+		Message message = new Message();
+		
+		if (listPerson != null && !listPerson.isEmpty()) {
+			message.setId(0);
+			message.setMessage("notEmpty");
+		} else {
+			message.setId(1);
+			message.setMessage("empty");
+		}
+		message.setListPerson(listPerson);
+
+		return new ResponseEntity<Message>(message, HttpStatus.OK);
+	}
+
+	@PostMapping(path = "/addPerson", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Message> addPerson(@RequestBody PersonDTO personDTO) {
+
+		HttpStatus httpStatus;
+		Message message = new Message();
+		if (personDTO != null) {
+			Person person = new Person();
+			person.setId(counter.getAndIncrement());
+			person.setName(personDTO.getName());
+			person.setAge(personDTO.getAge());
+			listPerson.add(person);
+			message.setId(1);
+			message.setMessage("success");
+			httpStatus = HttpStatus.CREATED;
+		} else {
+			message.setId(0);
+			message.setMessage("echec");
+			httpStatus = HttpStatus.NO_CONTENT;
+		}
+		return new ResponseEntity<Message>(message, httpStatus);
+	}
+
+	@PutMapping(path = "/updatePerson/{id}", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<Message> updatePerson(@PathVariable("id") int id, @RequestBody PersonDTO personDTO) {
+		HttpStatus httpStatus;
+		Message message = new Message();
+
+		Person person = listPerson.stream().filter(p -> id == p.getId()).findFirst().orElseGet(null);
+		if (person != null) {
+			person.setAge(personDTO.getAge());
+			person.setName(personDTO.getName());
+			message.setId(1);
+			message.setMessage("success");
+			httpStatus = HttpStatus.OK;
+		} else {
+			message.setId(0);
+			message.setMessage("echec");
+			httpStatus = HttpStatus.NO_CONTENT;
+		}
+		return new ResponseEntity<Message>(message, httpStatus);
+
+	}
+
+	@DeleteMapping(path = "/deletePerson/{id}")
+	public ResponseEntity<Message> deletePerson(@PathVariable("id") int id) {
+		HttpStatus httpStatus;
+		Message message = new Message();
+		boolean remove = listPerson.removeIf(p -> id == p.getId());
+		if (remove) {
+			message.setId(1);
+			message.setMessage("success");
+			httpStatus = HttpStatus.OK;
+
+		} else {
+
+			message.setId(0);
+			message.setMessage("echec");
+			httpStatus = HttpStatus.NOT_FOUND;
+		}
+
+		return new ResponseEntity<Message>(message, httpStatus);
+
+	}
+
+}
---
+++ Server/src/main/java/com/restService/Server/Message.java
@@ -0,0 +1,39 @@
+package com.restService.Server;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+@JsonInclude(Include.NON_NULL)
+public class Message {
+	
+	
+	private int id;
+	
+	private String message;
+	
+	private List<Person> listPerson;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public List<Person> getListPerson() {
+		return listPerson;
+	}
+	public void setListPerson(List<Person> listPerson) {
+		this.listPerson = listPerson;
+	}
+	
+	
+
+	
+	
+}
