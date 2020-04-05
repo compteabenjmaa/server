33a58ebeef3b97a80f0f54d1ff8206a9b4990c72
abenjmaa <benjmaa.abdelaziz@gmail.com>
2020-04-05 19:37:34 +0100

    cross domaine method config

--- Server/src/main/java/com/restService/Server/CRUDService.java
+++ Server/src/main/java/com/restService/Server/CRUDService.java
@@ -14,8 +14,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000" , methods = {RequestMethod.GET , RequestMethod.POST , RequestMethod.PUT , RequestMethod.DELETE})
 @RestController
 public class CRUDService {
 
